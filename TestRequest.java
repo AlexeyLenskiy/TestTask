eeba7f986147defc15ac39fcbadd6a9099d13591
Alexey <63260072+AlexeyLenskiy@users.noreply.github.com>
2021-04-12 23:29:27 +0300

    Add files via upload
    first commit

---
+++ TestRequest.java
@@ -0,0 +1,27 @@
+
+import java.sql.*;
+
+public class TestRequest {
+    public static final String URL = "jdbc:postgresql://localhost:5433/task";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "4e-Guev@r";
+    static String select = "SELECT department, SUM(salary) as salary FROM employees GROUP BY department";
+
+    public static void selectData() {
+        try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD )) {
+            PreparedStatement st = conn.prepareStatement(select);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString("department") + ": " + rs.getInt("salary"));
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        selectData();
+    }
+}
